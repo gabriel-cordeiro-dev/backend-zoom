c8d7a1b5ffc051025fbc10a278e359a62779948d
Gabriel Cordeiro <gabriel.cord18@gmail.com>
2023-07-30 16:12:13 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use('/api', routes);
 
 // Conecte ao banco de dados antes de iniciar o servidor
