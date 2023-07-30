7e39ea44bafcdfdb1f0c08d22fc76c2beef55b2b
Gabriel Cordeiro <gabriel.cord18@gmail.com>
2023-07-30 16:15:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+app.use(cors())
 
 app.use('/api', routes);
 
